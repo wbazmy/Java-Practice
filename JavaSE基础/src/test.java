40848630389b99e805832f67298780fc44e284b5
wbazmy <1760508765@qq.com>
2021-07-26 15:14:26 +0800

    Create test.java

---
+++ JavaSE基础/src/test.java
@@ -0,0 +1,10 @@
+/**
+ * @author Zhang Yang
+ * @description
+ * @date 2021/7/26 - 15:12
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
