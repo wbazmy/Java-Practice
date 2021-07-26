aa92018bb1e017939689e8f8369c0a1e1c1121e6
wbazmy <1760508765@qq.com>
2021-07-26 16:58:19 +0800

    Update test.java

--- JavaSE基础/src/test.java
+++ JavaSE基础/src/test.java
@@ -5,6 +5,8 @@
  */
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        String a="abc";
+        String b="abc";
+        System.out.println(a==b);
     }
 }
